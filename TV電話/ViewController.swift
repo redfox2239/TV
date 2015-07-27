c093b1b00bc405aaf91e2c6897753a5a41c5696b
HARADA REO <rharada@golyat.local>
2015-07-27 22:32:40 +0900

    Initial Commit

---
+++ TV電話/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TV電話
+//
+//  Created by HARADA REO on 2015/07/27.
+//  Copyright (c) 2015年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
